efa683ea6d4ac8e61c9033c6bfddf8f3d3c057d9
Joao Pedro <joaoapedroalvesdasilva26@gmail.com>
2025-06-14 21:00:09 -0300

    Position

--- chess-system/src/Program/Application.java
+++ chess-system/src/Program/Application.java
@@ -1,7 +1,11 @@
-package Program;
+package program;
+
+import boardgame.Position;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Sayonara");
+
+        Position pos = new Position(3,5);
+        System.out.println(pos);
     }
 }
